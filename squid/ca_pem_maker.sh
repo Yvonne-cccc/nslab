cc55974f78240bc7283fd9f4f5c36757c988ff25
Theo Stroth <Theo@TS-new>
2026-06-25 20:38:42 +0200

    information for squid transparent web proxy

---
+++ squid/ca_pem_maker.sh
@@ -0,0 +1,8 @@
+openssl genrsa -out squidCA.key 4096
+
+openssl req -new -x509 -days 3650 -key squidCA.key -out squidCA.crt
+
+cat squidCA.key squidCA.crt > ca.pem
+
+sudo chmod 400 ca.pem
+sudo chown proxy:proxy ca.pem
